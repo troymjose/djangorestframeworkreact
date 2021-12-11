3e03415374ba4b98c184a358ba02fdfd1c3e36fd
alenj <alenjacob.vinu@gmail.com>
2021-12-11 21:08:10 +0530

    Added urls and configs for documentation

--- troy/troy/urls.py
+++ troy/troy/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 from rest_framework_simplejwt.views import (
@@ -22,10 +24,32 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TROY REST API",
+        default_version='v1',
+        description="Code Challenge - Full Stack Developer",
+        terms_of_service="No Terms and Conditions",
+        contact=openapi.Contact(email="troymjose@gmail.com"),
+        license=openapi.License(name="Troy License"),
+    ),
+    public=True,
+    authentication_classes=(JWTAuthentication,),
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/products/', include('products.urls'), name='products'),
+    path('api/swagger.json/',
+         schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 ]
